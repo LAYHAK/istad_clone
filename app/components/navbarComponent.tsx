fb25f45bfe71691b6d9f5be3e7c5c82d7a639b68
LAYHAK <layhak.garu@gmail.com>
2024-03-17 21:50:01 +0700

    first init

---
+++ app/components/navbarComponent.tsx
@@ -0,0 +1,68 @@
+'use client'
+import {Navbar} from 'flowbite-react';
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import cstad from '../../public/images/logo/cstad.png';
+
+const customTheme = {
+    root: {
+        inner: {
+            base: 'mx-auto flex lg:gap-32 flex-wrap justify-between  lg:items-center lg:justify-start ',
+        }
+    },
+};
+const customLink = {
+    active: {
+        off: 'text-blue-50 font-extrabold',
+    }
+}
+const customBrand = {
+    base: 'flex gap-2 items-center justify-center',
+};
+const toggle = {
+    base: 'flex items-center justify-center  rounded-lg p-2 text-sm text-blue-50  focus:outline-none focus:ring-2 focus:ring-blue-200 dark:text-blue-400 dark:focus:ring-blue-600 lg:hidden'
+}
+const collapse = {
+    base: 'w-full lg:block lg:w-auto',
+    list: 'mt-4 flex flex-col md:mt-0 lg:flex-row lg:space-x-8 md:justify-center md:text-sm md:font-medium',
+}
+const NavbarComponent = () => {
+    return (
+
+        <Navbar fluid theme={customTheme} className="sticky lg:ps-10 top-0 z-50 bg-blue-800  ">
+            <Navbar.Brand as={Link} href="https://flowbite-react.com" theme={customBrand} className="lg:ps-10">
+                <Image src={cstad} alt="ISTAD" width={45} height={45}/>
+                <span
+                    className="self-center whitespace-nowrap text-blue-50 text-2xl font-extrabold ">CSTAD</span>
+            </Navbar.Brand>
+            <Navbar.Toggle theme={toggle}/>
+            <Navbar.Collapse theme={collapse}>
+                <Navbar.Link href="#" className="text-lg text-blue-50 font-extrabold text-center lg:text-start"
+                             theme={customLink}>
+                    Home
+                </Navbar.Link>
+                <Navbar.Link as={Link} className="text-lg text-blue-50 font-extrabold text-center lg:text-start "
+                             theme={customLink} href="#">
+                    Enroll
+                </Navbar.Link>
+                <Navbar.Link className="text-lg text-blue-50 font-extrabold text-center lg:text-start "
+                             theme={customLink}
+                             href="#">Course</Navbar.Link>
+                <Navbar.Link className="text-lg text-blue-50 font-extrabold text-center lg:text-start"
+                             theme={customLink}
+                             href="#">IT News</Navbar.Link>
+                <Navbar.Link className="text-lg text-blue-50 font-extrabold text-center lg:text-start"
+                             theme={customLink}
+                             href="#">Job Opportunity</Navbar.Link>
+                <Navbar.Link className="text-lg text-blue-50 font-extrabold text-center lg:text-start"
+                             theme={customLink}
+                             href="#">About Us</Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+
+
+};
+
+export default NavbarComponent;
---
+++ app/components/slide.tsx
@@ -0,0 +1,104 @@
+'use client'
+import {Image, Paper, rem, Text, Title, useMantineTheme} from '@mantine/core';
+import {Carousel} from '@mantine/carousel';
+import {FaArrowLeft, FaArrowRight} from "react-icons/fa";
+import classes from "../css/slide.module.css";
+import {useRef} from "react";
+import Autoplay from "embla-carousel-autoplay";
+import {Button} from "flowbite-react";
+
+const data = [
+    /* ... urls of images */
+    {
+        img: '/images/slide/slide1.png',
+        title: 'Center of Science and Technology Advanced Development',
+        desc: 'CSTAD is a noteworthy science and technology center in Cambodia. CSTAD has routed Cambodian students to advanced science and technology, research, and develop digital skills and our graduates have been guaranteed excellent job opportunities with the Top IT company.'
+    },
+    {
+        img: '/images/slide/slide2.png',
+        title: 'Mission',
+        desc: 'Provide the latest methodology with high-quality training and mentoring\n' +
+            '\n' +
+            'Build up the capacity and career of IT experts Cambodia\n' +
+            '\n' +
+            'Consult and connect CSTAD trainees to top IT careers'
+    },
+    {
+        img: '/images/slide/slide3.png',
+        title: 'Vision',
+        desc: 'Advanced IT Center in Cambodia'
+    }
+]
+
+interface CardProps {
+    img: string;
+    title: string;
+    desc: string;
+}
+
+const buttonColor = {
+    color: {
+        blue: 'text-white bg-blue-700 border border-transparent enabled:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 opacity-60 hover:opacity-100 ms-20',
+    }
+}
+
+function Card({img, title, desc}: CardProps) {
+    return (
+        <Paper
+            shadow="md"
+            p="xl"
+            radius="unset"
+            className={classes.card}
+        >
+            <div className="ms-20 pb-4">
+                <Image src={img} alt={title} className={classes.bg}/>
+                <Title order={3} className={classes.title}>
+                    {title}
+                </Title>
+                <Text className={classes.category} size="md">
+                    {desc}
+                </Text>
+            </div>
+            <Button color="blue" theme={buttonColor}>
+                Enroll now
+            </Button>
+
+        </Paper>
+    );
+}
+
+const Slide = () => {
+    const theme = useMantineTheme();
+
+    const autoplay = useRef(Autoplay({delay: 5000}));
+    const slides = data.map((item) => {
+        return (
+            <Carousel.Slide key={item.title}>
+                {/* eslint-disable-next-line jsx-a11y/alt-text,react/no-children-prop */}
+                <Card {...item}/>
+            </Carousel.Slide>
+        )
+    });
+
+
+    return <Carousel
+        withIndicators height={600}
+        nextControlIcon={
+            <FaArrowRight style={{width: rem(30), height: rem(30), color: 'white'}}/>
+        }
+        previousControlIcon={
+            <FaArrowLeft style={{width: rem(30), height: rem(30), color: 'white'}}/>
+        }
+        controlSize={40}
+        classNames={{indicator: classes.indicator}}
+        loop
+        plugins={[autoplay.current]}
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+    >
+        {slides}
+    </Carousel>;
+
+}
+
+export default Slide;
